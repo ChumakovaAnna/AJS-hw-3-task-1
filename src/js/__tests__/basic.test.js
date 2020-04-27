dfbf52b0b02b61baed67e647f15640d8dd2ec9bd
Anna Chumakova <chumakova3008@gmail.com>
2020-04-27 23:53:21 +0500

    Fixed error in test

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -2,11 +2,11 @@ import getHealthStatus from '../basic';
 
 describe('GetHealthStatus', () => {
   test.each([
-    [{ name: 'Mag', health: 90 }, 'healthy'],
-    [{ name: 'Mag', health: 10 }, 'critical'],
-    [{ name: 'Mag', health: 50 }, 'wounded'],
-    [{ name: 'Mag', health: 15 }, 'wounded'],
-  ])('for %s health get %i', (person, expected) => {
+    ['90 health get healthy', { name: 'Mag', health: 90 }, 'healthy'],
+    ['10 health get critical', { name: 'Mag', health: 10 }, 'critical'],
+    ['50 health get wounded', { name: 'Mag', health: 50 }, 'wounded'],
+    ['15 health get wounded', { name: 'Mag', health: 15 }, 'wounded'],
+  ])('%s', (name, person, expected) => {
     const result = getHealthStatus(person);
 
     expect(result).toBe(expected);
